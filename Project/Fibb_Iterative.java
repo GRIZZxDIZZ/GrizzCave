a57523489be43cbd8918b5aeeca47e692a48f1a3
Block Lee <dguens@gmail.com>
2021-04-04 23:15:22 -0400

    New files

--- Project/Fibb_Iterative.java
+++ Project/Fibb_Iterative.java
@@ -3,32 +3,39 @@ import java.util.Scanner;
 public class Fibb_Iterative {
 	
 static int Fibb_Iterate(int n) {
+	//base case
 		if(n == 0)return 0;
+		
 		int init = 0; 
 		int prev = 1;
 		int curr;
+		//for n times, a fibonacci number is calculated
 		for(int j = 1; j < n; j++) {
 			curr= init+ prev;	
 			init = prev;
 			prev = curr;
 		}
+		//returns nth fib number
 		return prev;
 	}
 
 	public static void main(String[] args) {
 		int x;
 		Scanner num = new Scanner(System.in);
-		
+		//enter n
 		System.out.println("enter a number");
 		x = num.nextInt();
+		//start timer
 		long starttime = System.nanoTime();
+		//run the fib sequence for each k, x times
 		for (int k = 0; k < x; k++) {
 			Fibb_Iterate(k);
 			System.out.println(Fibb_Iterate(k));
 		}
+		//stop timer and calculate time elapsed
 		long endtime = System.nanoTime();
 		long elapseTime = endtime-starttime;
-		
+		//print results
 		System.out.println("Elapsed Time: " +elapseTime+" nanoseconds" );
 	}
 
--- Project/TextAnalyzer.java
+++ Project/TextAnalyzer.java
@@ -1,5 +1,5 @@
-import java.io.File;
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,22 +7,102 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.StringTokenizer;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-public class TextAnalyzer {
+import javax.swing.*;
 
-	public static void main(String[] args)  {
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
+/**
+ * <h1>Text Analyzer</h1>
+ * This function uses jsoup to read an html file and 
+ * collect the top 20 words used in the file
+ * @author Guens Delius
+ * @version 1.0
+ * @since 2021-03
+ *
+ */
+public class TextAnalyzer extends JFrame {
+	
+	static int count = 0;
+	
+	int maxCount=0;
+	static int rank = 1;
+    String word = "";
+	private static JTable table;
+	static JScrollPane scrollPane = new JScrollPane(table);
+	
+	
+	
+	static Map<String, Long> newMap;
+	
+	private static Map<String, Long> valueSort(Map<String, Long> messMap){
+		/**
+	     * this function sorts a map using value comparisons
+	     * and the collections functions, returning a map
+		 */
 		
-		int count =0 , maxCount=0, rank = 1;
-		String word = "";
+		List<Map.Entry<String, Long>> list =
+				new LinkedList<Map.Entry<String, Long>>(messMap.entrySet());
+		
+		Collections.sort(list, new Comparator<Map.Entry<String, Long>>(){
+			public int compare(Map.Entry<String, Long> o1,
+							   Map.Entry<String, Long> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+		
+		Map<String, Long> sortMap = new LinkedHashMap<String, Long>();
+		for (Entry<String, Long> entry : list) {
+			sortMap.put(entry.getKey(), entry.getValue());
+		}
+		
+		return sortMap;
+			
+		}
+	public JTable makeTable(Map<String, Long> map) {
+		/**
+	     *This function takes in a Map and uses it to make a table
+		 */
+		
+		 System.out.println("Window Made");
+		setLayout(new FlowLayout());
+		
+		String colNames[] = {"Word", "Rank"};
+		Object [][] data = new Object[map.size()][2];
+		System.out.println("Window Made");
+		int i =0;
+		
+		for(Map.Entry<String, Long>entry : map.entrySet()) {
+			while(i != 20) {
+				data[i][0] = entry.getKey();
+				data[i][1] = entry.getValue();
+			}
+		}
+		System.out.println("Window Made");
+		JTable topTwenty = new JTable(data,colNames);
+		topTwenty.setPreferredScrollableViewportSize(new Dimension(500, 50));
+		topTwenty.setFillsViewportHeight(true);
+		System.out.println("Window Made");
+		return topTwenty;
+	}
+	
+	public static void main(String[] args)  {
+		/**
+	     *This is the main function
+		 */
+		
+		TextAnalyzer txt = new TextAnalyzer();
 		
 		 Document htmlFile = null;
 	        try {
@@ -37,7 +117,6 @@ public class TextAnalyzer {
 	        
 	        StringTokenizer token = new StringTokenizer(cssClass, " \"\t\n\r\f.:;?!—,");
 	 
-	        System.out.println("Jsoup can also parse HTML file directly");
 	        System.out.println("title : " + title);
 	        
 	        List<String> pool = new ArrayList<String>();
@@ -48,25 +127,26 @@ public class TextAnalyzer {
 	        }
 	               
 	       Map<String, Long> freq = Arrays.stream(pool.toString().split(" +|\n")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-	       LinkedHashMap<String, Long> wordsByCount = freq.entrySet()
-	    		   .stream()
-	    		   .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-	    		   .collect(Collectors.toMap(
-	    				   Map.Entry::getKey,
-	    				   Map.Entry::getValue,
-	    				   (v1, v2) ->{
-	    					   throw new IllegalStateException();
-	    				   },
-	    				   LinkedHashMap::new
-	    				   ));
+
+	       newMap = valueSort(freq);
+	       Iterator<Map.Entry<String, Long>> itr = newMap.entrySet().iterator();
+	       Map.Entry<String, Long> entry = itr.next();
+	      
+	       table = txt.makeTable(newMap);
+	       txt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       txt.setSize(600, 200);
+	       txt.setVisible(true);
+	       txt.setTitle("Java Table");
+	        
+	       while (itr.hasNext()) {
+	    	   while (rank != 21) {
+	    	   System.out.println("#"+ rank+": "+entry.getKey()+" "+entry.getValue());
+	    	   entry = itr.next();
+	    	   rank++;
+	           }
+	     }
 	       
-	    Iterator wbcIterator = wordsByCount.entrySet().iterator();
-	       System.out.println("Top 20 words");
-	       while(rank != 21) {
-	    	   System.out.println("#"+ rank +":"+ wbcIterator.next());
-			rank++;
-	        }
-}
+	}	
 }
 
 	
